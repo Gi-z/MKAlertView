e141e56489893af805bb0a4797de90e07ad5084e
Glenn Forbes <me@gizmoloon.com>
2013-10-14 09:55:43 +0100

    Create UIButton+MKAdditions.h

---
+++ UIButton+MKAdditions.h
@@ -0,0 +1,15 @@
+//
+//  UIButton+MKAdditions.h
+//  Metro
+//
+//  Created by Shawn Wall on 7/19/12.
+//  Copyright (c) 2012 TwoTap Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (MKAdditions)
+
+- (void)setBackgroundColor:(UIColor *)backgroundColor forState:(UIControlState)state;
+
+@end
