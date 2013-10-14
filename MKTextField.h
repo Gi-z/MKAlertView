13048df00de1fd10b57862f9b6ac2dd52c074a8c
Glenn Forbes <me@gizmoloon.com>
2013-10-14 09:57:46 +0100

    Create MKTextField.h

---
+++ MKTextField.h
@@ -0,0 +1,17 @@
+//
+//  MKTextField.h
+//  MetroKit
+//
+//  Created by Shawn Wall on 7/19/12.
+//  Copyright (c) 2012 TwoTap Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ Flattened text field with square edges. Default state of white background & 
+ black text. 
+ */
+@interface MKTextField : UITextField
+
+@end
